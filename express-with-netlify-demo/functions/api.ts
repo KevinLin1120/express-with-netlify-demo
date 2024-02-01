61192979ab5e28a3ccc496dd67aafdccfb889586
KevinLin1120 <kevinlin1120a@gmail.com>
2024-02-02 01:25:20 +0800

    Update api.ts

--- express-with-netlify-demo/functions/api.ts
+++ express-with-netlify-demo/functions/api.ts
@@ -10,9 +10,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const app = express();
 
-app.set('views', './views'); // 視圖文件的資料夾
-app.set('view engine', 'pug'); // 設定使用的渲染引擎（可參考 express 的 app.js）
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
