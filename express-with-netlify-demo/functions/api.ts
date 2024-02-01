779c0422ef825ab74fbd2c0b4f04f13a8cdc24ed
KevinLin1120 <kevinlin1120a@gmail.com>
2024-02-02 00:38:27 +0800

    Update api.ts

--- express-with-netlify-demo/functions/api.ts
+++ express-with-netlify-demo/functions/api.ts
@@ -5,10 +5,19 @@ import serverless from "serverless-http";
 var indexRouter = require('../routes/index');
 var userRouter = require('../routes/users');
 
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const app = express();
 
 app.set('views', './views'); // 視圖文件的資料夾
 app.set('view engine', 'pug'); // 設定使用的渲染引擎（可參考 express 的 app.js）
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
 app.use("/users/", userRouter);
