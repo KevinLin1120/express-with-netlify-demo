d1b38b379b31a81323148c9ab3820a9c7641ab2d
KevinLin1120 <kevinlin1120a@gmail.com>
2024-02-02 00:27:41 +0800

    Update api.ts

--- express-with-netlify-demo/functions/api.ts
+++ express-with-netlify-demo/functions/api.ts
@@ -7,8 +7,8 @@ var userRouter = require('../routes/users');
 
 const app = express();
 
-app.set('view engine', 'pug'); // 設定使用的渲染引擎（可參考 express 的 app.js）
 app.set('views', './views'); // 視圖文件的資料夾
+app.set('view engine', 'pug'); // 設定使用的渲染引擎（可參考 express 的 app.js）
 
 app.use("/", indexRouter);
 app.use("/users/", userRouter);
