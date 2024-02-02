3aea7b3fa2f34bd0af9618f27203da62e7dd438e
KevinLin1120 <kevinlin1120a@gmail.com>
2024-02-02 17:10:04 +0800

    Update index.js

--- express-with-netlify-demo/routes/index.js
+++ express-with-netlify-demo/routes/index.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var path = require("path");
+// 指定靜態文件目錄
+router.use(express.static('views'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname, '../../../../views', 'index.html'));
+  res.sendFile('index.html', {root: 'views'}, (err) => {
+    if (err) {
+      console.error(`Error sending file: ${err}`);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 });
 
 router.get('/html', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname, '../../../../views', 'test.html'));
+  res.sendFile('test.html', {root: 'views'}, (err) => {
+    if (err) {
+      console.error(`Error sending file: ${err}`);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 });
 
 module.exports = router;
